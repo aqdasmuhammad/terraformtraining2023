a175cb80f5e3b9e0d84c6ee6018a61b8769ee034
Muhammad Aqdas <maqdas@hiddenleaf.com>
2023-12-12 08:26:04 +0000

    terraform and docker

---
+++ docker-tf/nginx_main.tf
@@ -0,0 +1,28 @@
+terraform {  #using this block to mention sepcific resource providers
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" { //using this block to download and keep docker image locally
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
+/* using last blocl
+to specify configurations 
+for container run by docker */
