de0ffd92c41de1f1043f70bd5663027a7e78e711
Muhammad Aqdas <maqdas@hiddenleaf.com>
2023-12-13 07:27:17 +0000

    local-exec

--- http-tf/main.tf
+++ http-tf/main.tf
@@ -1,7 +1,7 @@
 # The following example shows how to issue an HTTP GET request supplying
 # an optional request header.
-data "http" "iss" {
-  url = "http://api.open-notify.org/astros.json"      // API to send HTTP GET to
+data "http" "pokemon" {
+  url = "https://pokeapi.co/api/v2/pokemon/charizard/"      // API to send HTTP GET to
 
   # Optional request headers
   request_headers = {
--- http-tf/outputs.tf
+++ http-tf/outputs.tf
@@ -1,11 +1,11 @@
 # produces an output value named "space_heroes"
-output "space_heroes" {
+output "pokedesksays" {
   description = "API that documents folks in space"
-  value       = data.http.iss.response_body
+  value       = data.http.pokemon.response_body
 }
 
 # produces legal JSON output value named "space_heroes_json"
-output "space_heroes_json" {
+output "pokedesksays_json" {
   description = "API that documents folks in space"
-  value       = jsondecode(data.http.iss.response_body)    // note the jsondecode()
+  value       = jsondecode(data.http.pokemon.response_body)    // note the jsondecode()
 }    
